0960759e4c8d1cdf074f27f02156a0bdac132b23
ruhuma <141302425+ruhuma@users.noreply.github.com>
2024-01-02 18:11:21 +0000

    4

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 @app.route('/')
 def Hello():
-    return('Hi')
+    return render_template('index.html')
+
+@app.route('/name', methods = ['GET','POST'])
+def hey():
+    if request.method == 'POST':
+        area = request.form['area']
+        print(area)
 
 if __name__ == '__main__':
     app.run()
